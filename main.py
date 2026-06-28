30bfd82168376ed7c9ed71a2885b07799312445b
Riccardo Rende <rrende@flatironinstitute.org>
2026-06-28 17:26:24 -0400

    Refactor code

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from flax import struct
 import flax.serialization as fs
 import jax.numpy as jnp
 import numpy as np
-from deepvmc.operator.spins import Sz, Sminus, Splus
-from deepvmc.state.lattice.spins import SpinState
+from deepvmc.lattice.operator.spins import Sz, Sminus, Splus
+from deepvmc.lattice.state.spins import SpinState
+import jax
 
 operator = Sz(3) * Sminus(0) * Splus(1) + Sminus(2) * Sz(3)
 # print(operator)
@@ -12,7 +13,10 @@ state = SpinState(spins=jnp.array([[1, -1, 1, -1], [1, 1, 1, 1], [-1, -1, 1, -1]
 
 result = operator(state)
 
-print(result)
+connected_states = result.connected_states
+result2 = jax.vmap(operator)(connected_states)
+
+
 # print(result["offdiagonal"]["new_state"].spins.shape)
 # print(result["offdiagonal"]["new_state"].spins.shape)
 # print(fs.to_state_dict(sz))
