ca11ff6febd3ac9a93683e333f718688e3656d49
Riccardo Rende <rrende@ccqlin046.flatironinstitute.org>
2026-07-02 20:37:57 -0400

    Simplify main

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from functools import partial
 
 import jax
 import jax.numpy as jnp
@@ -6,17 +5,9 @@ from tachys.lattice.ansatz.fermionic_transformer import FermionicTransformer
 from tachys.lattice.bond_exchange import BondExchange
 from tachys.lattice.fermions.fermion_state import FermionState, init_config_spinful
 from tachys.lattice.fermions.hamiltonians.hubbard import hubbard_hamiltonian
-from tachys.lattice.spins.hamiltonians.heisenberg import heisenberg_square_pbc
-from tachys.lattice.spins.hamiltonians.ising_transverse_field import ising_transverse_field_square_pbc
-from tachys.lattice.spins.spin_state import SpinState, init_config_fixed_magn
-from tachys.lattice.spins.spin_action import SpinFlip
 from tachys.lattice.lattice_database import square
-from tachys.montecarlo import CompositeAction
-from tachys.lattice.ansatz.spin_vit import SpinViT
-from tachys.optimizer.optimizers import _build_ntk as _build_ntk_base
-from tachys.parallel import mesh
 from tachys.wavefunction import WaveFunction
-from tachys.optimizer import SR, SPRING, MARCH
+from tachys.optimizer import MARCH
 from tachys.ground_state_training import train
 
 L = 4
@@ -26,8 +17,10 @@ Ne = 10
 N_mc = 512
 U = 8
 
+N_steps  = 10
+eta0     = 0.005
+
 key = jax.random.key(seed)
-# Heisenberg Hamiltonian on a 4x4 square lattice with PBC
 lattice = square(shape=(L, L))
 H = hubbard_hamiltonian(lattice, nn=[((1, 0), 1.0), ((0, 1), 1.0)], U=U)
 
@@ -48,35 +41,18 @@ model = FermionicTransformer(num_layers=2,
                  dtype=jnp.float64)
 
 # Initialise RBM parameters with a dummy forward pass
-params = model.init(jax.random.key(1), state)
-wf = WaveFunction(params=params, apply_fn=model.apply)
+key, subkey = jax.random.split(key)
+params = model.init(subkey, state)
+wf = WaveFunction(params=params, apply_fn=model.apply, dtype=jnp.float32)
 
 # MC sampling
 action = BondExchange.create(lattice, max_dist=1, Nbands=2)
 
-N_steps  = 1000
-eta0     = 0.005
-eta_final = 0.0005  # lr at step N_steps
-
-def lr_schedule(step):
-    """Linear decay from eta0 down to eta_final over N_steps."""
-    frac = min(step / N_steps, 1.0)
-    return eta0 + (eta_final - eta0) * frac
+lr_schedule = lambda step: eta0
 
 optimizer = MARCH(diag_shift=1e-4, mode="real")
 
-use_wandb = False
-wandb_run = None
-if use_wandb:
-    import wandb
-    wandb_run = wandb.init(project="tachys", config={
-        "L": L, "Ne": Ne, "N_mc": N_mc, "U": U,
-        "N_steps": N_steps, "eta0": eta0, "eta_final": eta_final,
-    })
-
+key, subkey = jax.random.split(key)
 key, state, wf, opt_state, history = train(
-    key, H, state, wf, optimizer, action, N_steps, lr_schedule, N_mc, wandb_run=wandb_run
+    subkey, H, state, wf, optimizer, action, N_steps, lr_schedule, N_mc
 )
-
-if wandb_run is not None:
-    wandb_run.finish()
