0899c0c645d37c30e9a68a681e1c0637bc1d1cdb
Riccardo Rende <rrende@ccqlin046.flatironinstitute.org>
2026-07-21 12:39:46 -0400

    Remove SpinExchange

--- tachys/lattice/spins/spin_action.py
+++ tachys/lattice/spins/spin_action.py
@@ -1,8 +1,5 @@
-from functools import partial
-
 import jax
 import jax.numpy as jnp
-from flax import struct
 
 from tachys.montecarlo import _BaseAction
 
@@ -13,38 +10,6 @@ def exchange_spins(spins, id1, id2):
     spins = spins.at[id1].set(spins[id2])
     spins = spins.at[id2].set(aus)
     return spins
-
-
-class SpinExchange(_BaseAction):
-    """Proposes swapping two spins separated by at most `max_dist` bonds.
-
-    The neighbour to swap with is drawn uniformly from all neighbours within
-    the requested distance, so the proposal is symmetric and
-    log_prob_correction = 0.
-
-    Attributes:
-        max_dist: Maximum bond distance between the two sites to exchange.
-    """
-
-    max_dist: int = struct.field(pytree_node=False)
-
-    def __call__(self, key, state):
-        rands = jax.vmap(partial(jax.random.uniform, shape=(3,)))(key)
-
-        n_mult = jnp.array(state.n_mult)
-        neighbours = jnp.array(state.neighbours)
-        N_mc_local = state.spins.shape[0]
-
-        index = (rands[:, 0] * state.Ns).astype(int)
-        dist_neigh = (rands[:, 1] * self.max_dist + 1).astype(int)
-        pos_neigh = (rands[:, 2] * n_mult[dist_neigh]).astype(int)
-        index_nn = neighbours[index, pos_neigh, dist_neigh]
-
-        allowed_move = state.spins[jnp.arange(N_mc_local), index] != state.spins[jnp.arange(N_mc_local), index_nn]
-        new_spins = jax.vmap(exchange_spins)(state.spins, index, index_nn)
-        log_prob_correction = 0.0
-
-        return state.replace(spins=new_spins), allowed_move, log_prob_correction
 
 
 class SpinFlip(_BaseAction):
